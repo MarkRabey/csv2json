e904fdf21ee8c0a396953fb5937e8e9869c9bd1c
Mark Rabey <mark.rabey@bounteous.com>
2022-02-23 09:12:51 -0500

    update program version number

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ program
   .name('c2j')
   .usage('[command] [options]')
   .description('CLI to parse CSV files and output JSON')
-  .version('0.1.4')
+  .version('0.2.0')
   .showHelpAfterError()
   .showSuggestionAfterError();
 
